1eee47c650ede62436e52c64c33d52f3eb832f47
Andreas19 <andreas19@posteo.eu>
2020-06-02 09:47:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Cython',
         'Topic :: Software Development :: Libraries',
     ],
     ext_modules=[Extension('cyprimes', ['src/cyprimes.c'])],
